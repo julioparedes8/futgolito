129dddba0eff29c8c17495be67999462bd6ee3f7
julioparedes8 <julioe.paredes4@outlook.com>
2018-11-01 10:30:24 -0700

    Configuración Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyC3rUyRvoxh6GAFPyHkuBKWTEqoNyyCvPQ",
+    authDomain: "futgolito-d81e6.firebaseapp.com",
+    databaseURL: "https://futgolito-d81e6.firebaseio.com",
+    projectId: "futgolito-d81e6",
+    storageBucket: "futgolito-d81e6.appspot.com",
+    messagingSenderId: "433825676594"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyC3rUyRvoxh6GAFPyHkuBKWTEqoNyyCvPQ",
+    authDomain: "futgolito-d81e6.firebaseapp.com",
+    databaseURL: "https://futgolito-d81e6.firebaseio.com",
+    projectId: "futgolito-d81e6",
+    storageBucket: "futgolito-d81e6.appspot.com",
+    messagingSenderId: "433825676594"
+  }
 };
 
 /*
