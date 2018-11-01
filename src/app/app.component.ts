759045e4f45ef9517af0123495510c6d24a33b46
julioparedes8 <julioe.paredes4@outlook.com>
2018-11-01 09:49:16 -0700

    prueba primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'futgolito';
+  showHead: boolean = false;
+  ngOnInit() {
+  }
+
+  constructor(private router: Router) {
+  // on route change to '/login', set the variable showHead to false
+    router.events.forEach((event) => {
+      if (event instanceof NavigationStart) {
+        if ((event['url'] == '/presidente') ||(event['url'] == '/torneo') ||(event['url'] == '/registrartorneo')||(event['url'] == '/equipo')||(event['url'] == '/registrarequipo')
+        ||(event['url'] == '/campo')||(event['url'] == '/registrarcampo')||(event['url'] == '/jugador')||(event['url'] == '/registrarjugador')) {
+          this.showHead = false;
+        } else {
+          // console.log("NU")
+          this.showHead = true;
+        }
+      }
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,68 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {Routes, RouterModule} from '@angular/router'
 import { AppComponent } from './app.component';
-
+import { InicioComponent } from './inicio/inicio.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { from } from 'rxjs';
+import { AboutComponent } from './about/about.component';
+import { LigasComponent } from './ligas/ligas.component';
+import { HeaderpresidenteComponent } from './headerpresidente/headerpresidente.component';
+import { IniciopresidenteComponent } from './iniciopresidente/iniciopresidente.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { IniciotorneoComponent } from './iniciotorneo/iniciotorneo.component';
+import { DataTablesModule } from 'angular-datatables';
+import { RegistrartorneoComponent } from './registrartorneo/registrartorneo.component';
+import { InicioequipoComponent } from './inicioequipo/inicioequipo.component';
+import { RegistrarequipoComponent } from './registrarequipo/registrarequipo.component';
+import { IniciocamposComponent } from './iniciocampos/iniciocampos.component';
+import { RegistrarcampoComponent } from './registrarcampo/registrarcampo.component';
+import { IniciojugadorComponent } from './iniciojugador/iniciojugador.component';
+import { RegistrarjugadorComponent } from './registrarjugador/registrarjugador.component';
+const routes :Routes =[
+  {path: '',component: InicioComponent},
+  {path: 'login',component: LoginComponent},
+  {path: 'ligas',component: LigasComponent},
+  {path: 'signup', component:SignupComponent},
+  {path: 'about', component:AboutComponent},
+  {path: 'presidente', component:IniciopresidenteComponent},
+  {path: 'torneo', component:IniciotorneoComponent},
+  {path: 'equipo', component:InicioequipoComponent},
+  {path: 'campo', component:IniciocamposComponent},
+  {path: 'jugador', component:IniciojugadorComponent},
+  {path: 'registrartorneo', component:RegistrartorneoComponent},
+  {path: 'registrarequipo', component:RegistrarequipoComponent},
+  {path: 'registrarcampo', component:RegistrarcampoComponent},
+  {path: 'registrarjugador', component:RegistrarjugadorComponent},
+  {path: '**',component: InicioComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    HeaderComponent,
+    LoginComponent,
+    SignupComponent,
+    AboutComponent,
+    LigasComponent,
+    HeaderpresidenteComponent,
+    IniciopresidenteComponent,
+    IniciotorneoComponent,
+    RegistrartorneoComponent,
+    InicioequipoComponent,
+    RegistrarequipoComponent,
+    IniciocamposComponent,
+    RegistrarcampoComponent,
+    IniciojugadorComponent,
+    RegistrarjugadorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    AngularFontAwesomeModule,
+    DataTablesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
