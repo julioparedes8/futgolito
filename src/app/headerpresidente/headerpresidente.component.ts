1b429edb544afb7f34bf1e814791d74b9aad1ac3
julioparedes8 <julioe.paredes4@outlook.com>
2018-11-01 12:42:10 -0700

    Añadimos login y logout

--- src/app/headerpresidente/headerpresidente.component.ts
+++ src/app/headerpresidente/headerpresidente.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {AuthService} from '../servicios/auth.service';
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-headerpresidente',
   templateUrl: './headerpresidente.component.html',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderpresidenteComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public authService: AuthService,
+    public router: Router) 
+    { }
 
   ngOnInit() {
   }
-
+  cerraSesion(){
+    this.authService.logOut()
+    .then ((res)=>{
+      this.router.navigate(['/inicio']);
+    }).catch((err)=>{
+      console.log(err);
+    });
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import {AuthService} from '../servicios/auth.service';
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
+  public email: string;
+  public password: string;
+  constructor(
+    public authService: AuthService,
+    public router: Router
+  ) { }
   ngOnInit() {
   }
-
+  //metodo
+  iniciarSesion(){
+    this.authService.logIn(this.email,this.password)
+    .then ((res) => {
+      this.router.navigate(['/presidente']);
+    }).catch((err)=>{
+      console.log(err);
+      this.router.navigate(['/login']);
+    })
+  }
 }
